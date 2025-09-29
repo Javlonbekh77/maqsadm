ca9c1d0785a02ce47e323247c1cd9b71efb35069
Javlonbek Xoliqulov <javlonbekh2007@gmail.com>
2025-09-29 07:38:42 +0000

    Try fixing this error: `Console Error: A `locale` is expected to be retu

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,15 @@
 
 import createMiddleware from 'next-intl/middleware';
-import {locales, pathnames} from './navigation';
-
+ 
 export default createMiddleware({
   // A list of all locales that are supported
-  locales,
+  locales: ['en', 'uz'],
+ 
   // Used when no locale matches
-  defaultLocale: 'uz',
-  localePrefix: 'always',
-  pathnames,
+  defaultLocale: 'uz'
 });
-
+ 
 export const config = {
   // Match only internationalized pathnames
-  matcher: [
-    // Match all pathnames except for
-    // - … if they start with `/api`, `/_next` or `/_vercel`
-    // - … the ones containing a dot (e.g. `favicon.ico`)
-    '/((?!api|_next|_vercel|.*\\..*).*)',
-  ]
+  matcher: ['/', '/(uz|en)/:path*']
 };
---
+++ src/next.config.ts
@@ -0,0 +1,44 @@
+import type {NextConfig} from 'next';
+import withNextIntl from 'next-intl/plugin';
+ 
+const withNextIntlConfig = withNextIntl(
+  // This is the default location of the configuration object
+  './src/app/i18n.ts'
+);
+
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: ["*.cloudworkstations.dev"],
+  }
+};
+
+export default withNextIntlConfig(nextConfig);
